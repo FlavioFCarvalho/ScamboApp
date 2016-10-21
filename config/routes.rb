886371f2bf1751540afc7e9800e74b058bf32ab0
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-10-21 10:01:10 -0200

    Arrumando a rota

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
   get 'home/index'
-  root 'home#index' 
+  root 'site/home#index' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
